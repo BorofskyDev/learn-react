fb25c13c4a6ec2a612354c2083732c51657580fc
solovyovsdom <joelborofsky@mac.com>
2022-11-05 19:48:48 -0500

    redid class component into function hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Counter from './Counter'
-import './App.css'
+import Counter from './Counter';
+import './App.css';
+import CounterHooks from './CounterHooks';
 
 function App() {
-
-
   return (
-   <Counter initialCount={0} />
-  )
+    <>
+      <h1>Class Counter</h1>
+      <Counter initialCount={0} />
+      <br />
+      <h1>Function Counter with Hooks</h1>
+      <CounterHooks initialCount={0} /> 
+    </>
+  );
 }
 
-export default App
+export default App;
