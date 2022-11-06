4127608859144d04973af7e25133768dc947e759
solovyovsdom <joelborofsky@mac.com>
2022-11-05 20:26:42 -0500

    set up usecontext hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
 import Counter from './Counter';
 import './App.css';
 import CounterHooks from './CounterHooks';
 
+
+export const ThemeContext = React.createContext();
+
 function App() {
+  const [theme, setTheme] = useState('red')
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       <h1>Class Counter</h1>
       <Counter initialCount={0} />
       <br />
       <h1>Function Counter with Hooks</h1>
-      <CounterHooks initialCount={0} /> 
-    </>
+      <CounterHooks initialCount={0} />
+     <button onClick={() => setTheme(prevTheme => {
+      return prevTheme === "red" ? 'blue' : 'red'
+     })} >Change Theme</button>
+    </ThemeContext.Provider>
   );
 }
 
